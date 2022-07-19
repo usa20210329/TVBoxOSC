217175c9d91cc3341f281614965221b70ef3378a
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-19 17:41:13 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -319,6 +319,7 @@ public class ModelSettingFragment extends BaseLazyFragment {
                 players.add(0);
                 players.add(1);
                 players.add(2);
+                players.add(7);
                 players.add(8);
                 players.add(9);
                 players.add(10);
