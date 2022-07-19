c31e3a6837eed2fccb42835688e8d81d8b47f64a
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-19 20:37:43 +0800

    Create DangBeiKeyEvent.java

---
+++ app/src/main/java/com/github/tvbox/osc/util/DangBeiKeyEvent.java
@@ -0,0 +1,55 @@
+package com.github.tvbox.osc.util;
+/**
+ * 当贝投影按键.
+ * @author anaer
+ */
+public class DangBeiKeyEvent {
+    /**
+     * 上.
+     */
+   public static final int UP = 19; 
+   /**
+    * 下.
+    */
+   public static final int DOWN = 20; 
+   /**
+    * 左.
+    */
+   public static final int LEFT = 21; 
+   /**
+    * 右.
+    */
+   public static final int RIGHT = 22; 
+
+   /**
+    * OK
+    */
+   public static final int OK = 23;
+   /**
+    * 取消
+    */
+   public static final int CANCEL = 4;
+
+   /**
+    * 菜单键.
+    */
+   public static final int MENU = 82;
+
+   /**
+    * 音量增
+    */
+   public static final int VOL_UP = 24;
+   /**
+    * 音量减
+    */
+   public static final int VOL_DOWN = 25;
+
+   /**
+    * 语音控制
+    */
+   public static final int SIRI = 135;
+   /**
+    * 侧边菜单键 (控制当贝设置)
+    */
+   public static final int SIDE = 136;
+}
