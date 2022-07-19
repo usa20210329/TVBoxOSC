9b33bd16c8006cd3835c40103c86c91117117ddd
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-19 21:45:31 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -51,7 +51,6 @@ public class ModelSettingFragment extends BaseLazyFragment {
     private TextView tvMediaCodec;
     private TextView tvParseWebView;
     private TextView tvPlay;
-    private TextView tvLivePlay;
     private TextView tvRender;
     private TextView tvScale;
     private TextView tvApi;
@@ -79,7 +78,6 @@ public class ModelSettingFragment extends BaseLazyFragment {
         tvParseWebView = findViewById(R.id.tvParseWebView);
         tvMediaCodec = findViewById(R.id.tvMediaCodec);
         tvPlay = findViewById(R.id.tvPlay);
-        tvLivePlay = findViewById(R.id.tvLivePlay);
         tvRender = findViewById(R.id.tvRenderType);
         tvScale = findViewById(R.id.tvScaleType);
         tvApi = findViewById(R.id.tvApi);
@@ -97,9 +95,7 @@ public class ModelSettingFragment extends BaseLazyFragment {
         tvHomeApi.setText(ApiConfig.get().getHomeSourceBean().getName());
         tvScale.setText(PlayerHelper.getScaleName(Hawk.get(HawkConfig.PLAY_SCALE, 0)));
         tvPlay.setText(PlayerHelper.getPlayerName(Hawk.get(HawkConfig.PLAY_TYPE, 0)));
-        tvLivePlay.setText(PlayerHelper.getPlayerName(Hawk.get(HawkConfig.LIVE_PLAY_TYPE, 0)));
         tvRender.setText(PlayerHelper.getRenderName(Hawk.get(HawkConfig.PLAY_RENDER, 0)));
-        tvChannelHint.setText(Hawk.get(HawkConfig.CHANNEL_HINT, ""));
         findViewById(R.id.llDebug).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -376,63 +372,6 @@ public class ModelSettingFragment extends BaseLazyFragment {
             }
         });
         
-        findViewById(R.id.llLivePlay).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FastClickCheckUtil.check(v);
-                int defaultPos = Hawk.get(HawkConfig.LIVE_PLAY_TYPE, 0);
-                ArrayList<Integer> players = new ArrayList<>();
-                players.add(Constants.PLAYER_SYSTEM);
-                players.add(Constants.PLAYER_IJK);
-                players.add(Constants.PLAYER_EXO);
-                
-                if(DangbeiPlayer.isAvailable()){
-                    players.add(Constants.PLAYER_DANGBEI);
-                }
-
-                if(ucplayer.isAvailable()){
-                    players.add(Constants.PLAYER_UC);
-                }
-
-                if(browser.isAvailable()){
-                    players.add(Constants.PLAYER_BROWER);
-                }
-
-                if(ReexPlayer.isAvailable()){
-                    players.add(Constants.PLAYER_REEX);
-                }
-
-                if(MXPlayer.isAvailable()){
-                    players.add(Constants.PLAYER_MX);
-                }
-                SelectDialog<Integer> dialog = new SelectDialog<>(mActivity);
-                dialog.setTip("请选择默认播放器");
-                dialog.setAdapter(new SelectDialogAdapter.SelectDialogInterface<Integer>() {
-                    @Override
-                    public void click(Integer value, int pos) {
-                        Hawk.put(HawkConfig.LIVE_PLAY_TYPE, value);
-                        tvLivePlay.setText(PlayerHelper.getPlayerName(value));
-                        PlayerHelper.init();
-                    }
-
-                    @Override
-                    public String getDisplay(Integer val) {
-                        return PlayerHelper.getPlayerName(val);
-                    }
-                }, new DiffUtil.ItemCallback<Integer>() {
-                    @Override
-                    public boolean areItemsTheSame(@NonNull @NotNull Integer oldItem, @NonNull @NotNull Integer newItem) {
-                        return oldItem.intValue() == newItem.intValue();
-                    }
-
-                    @Override
-                    public boolean areContentsTheSame(@NonNull @NotNull Integer oldItem, @NonNull @NotNull Integer newItem) {
-                        return oldItem.intValue() == newItem.intValue();
-                    }
-                }, players, defaultPos);
-                dialog.show();
-            }
-        });        
         findViewById(R.id.llRender).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
