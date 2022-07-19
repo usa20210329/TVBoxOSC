68ec8859498d3e81a73bf528291614daaafb879d
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-19 17:54:59 +0800

    Update MXPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/thirdparty/MXPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/thirdparty/MXPlayer.java
@@ -53,6 +53,13 @@ public class MXPlayer {
         }
         return null;
     }
+    /**
+     * 判断是否可用.
+     * @return
+     */
+    public static boolean isAvailable(){
+        return getPackageInfo() != null;
+    }
 
     private static class Subtitle {
         final Uri uri;
