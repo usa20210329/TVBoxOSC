8291d2444d7304d749b61dc6810fc787ca31a055
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-19 17:51:29 +0800

    Create Constants.java

---
+++ app/src/main/java/com/github/tvbox/osc/constant/Constants.java
@@ -0,0 +1,41 @@
+package com.github.tvbox.osc.constant;
+
+/**
+ * 常量定义.
+ */
+public class Constants {
+
+    /**
+     * 系统播放器.
+     */
+    public static final int PLAYER_SYSTEM = 0;
+    /**
+     * IJK播放器.
+     */
+    public static final int PLAYER_IJK = 1;
+    /**
+     * EXO播放器.
+     */
+    public static final int PLAYER_EXO = 2;
+      /**
+     * 当贝播放器.
+     */
+    public static final int PLAYER_DANGBEI = 7;
+      /**
+     * UC播放
+     */
+    public static final int PLAYER_UC = 8;
+      /**
+     * 浏览器播放
+     */
+    public static final int PLAYER_BROWER = 9;
+    /**
+     * MX播放器
+     */
+    public static final int PLAYER_MX= 10;
+    /**
+     * Reex播放器.
+     */
+    public static final int PLAYER_REEX= 11;
+
+}
