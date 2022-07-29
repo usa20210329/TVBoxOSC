68410ea8913d29eff0e445c00481f3df81bcebd2
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-29 22:54:15 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -359,13 +359,12 @@ public class ApiConfig {
                         extUrlFix = clanContentFix(clanToAddress(apiUrl), extUrlFix);
                         extUrlFix = Base64.encodeToString(extUrlFix.getBytes("UTF-8"), Base64.DEFAULT | Base64.URL_SAFE | Base64.NO_WRAP);
                         url = url.replace(extUrl, extUrlFix);
-                    }
-                    else if (extUrlFix.startsWith("asset://")) {
-                        extUrlFix = readAssetsText(apiUrl.replace("asset://", extUrlFix));
+                     else if (extUrlFix.startsWith("asset://")) {
+                        extUrlFix = readAssetsText(extUrlFix);
                         extUrlFix = Base64.encodeToString(extUrlFix.getBytes("UTF-8"), Base64.DEFAULT | Base64.URL_SAFE | Base64.NO_WRAP);
                         url = url.replace(extUrl, extUrlFix);
-                    }
-                        
+                       }   
+                    } 
                 }
                 LiveChannelGroup liveChannelGroup = new LiveChannelGroup();
                 liveChannelGroup.setGroupName(url);
