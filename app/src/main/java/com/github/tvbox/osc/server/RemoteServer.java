14b5f658c0d2d8b0c7092af1c07406b217cca474
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-07-30 10:05:15 +0800

    Update RemoteServer.java

--- app/src/main/java/com/github/tvbox/osc/server/RemoteServer.java
+++ app/src/main/java/com/github/tvbox/osc/server/RemoteServer.java
@@ -70,6 +70,7 @@ public class RemoteServer extends NanoHTTPD {
         getRequestList.add(new RawRequestProcess(this.mContext, "/jquery.js", R.raw.jquery, "application/x-javascript"));
         getRequestList.add(new RawRequestProcess(this.mContext, "/script.js", R.raw.script, "application/x-javascript"));
         getRequestList.add(new RawRequestProcess(this.mContext, "/favicon.ico", R.drawable.app_icon, "image/x-icon"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/live.txt", R.raw.live, "text/txt"));
     }
 
     private void addPostRequestProcess() {
