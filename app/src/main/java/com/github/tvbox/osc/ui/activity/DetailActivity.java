d1dc547eb78bc9fb91b68f7961e5aa464971142a
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-08-02 09:34:19 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.github.tvbox.osc.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Rect;
@@ -61,6 +62,7 @@ import org.json.JSONObject;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.TreeSet;
@@ -168,6 +170,7 @@ public class DetailActivity extends BaseActivity {
         },500);
         tvPlay.requestFocus();
         tvSort.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onClick(View v) {
                 if (vodInfo != null && vodInfo.seriesMap.size() > 0) {
@@ -284,7 +287,7 @@ public class DetailActivity extends BaseActivity {
 
             @Override
             public void onItemPreSelected(TvRecyclerView parent, View itemView, int position) {
-                //seriesSelect = false;
+                seriesSelect = false;
             }
 
             @Override
@@ -543,8 +546,8 @@ public class DetailActivity extends BaseActivity {
 
     private String searchTitle = "";
     private boolean hadQuickStart = false;
-    private List<Movie.Video> quickSearchData = new ArrayList<>();
-    private List<String> quickSearchWord = new ArrayList<>();
+    private final List<Movie.Video> quickSearchData = new ArrayList<>();
+    private final List<String> quickSearchWord = new ArrayList<>();
     private ExecutorService searchExecutorService = null;
 
     private void switchSearchWord(String word) {
@@ -719,9 +722,9 @@ public class DetailActivity extends BaseActivity {
     boolean fullWindows = false;
     ViewGroup.LayoutParams windowsPreview = null;
     ViewGroup.LayoutParams windowsFull = null;
-    ViewGroup playerParent = null;
-    View playerRoot = null;
-    ViewGroup llLayoutParent = null;
+//    ViewGroup playerParent = null;
+//    View playerRoot = null;
+//    ViewGroup llLayoutParent = null;
 
     void toggleFullPreview() {
         if (windowsPreview == null) {
@@ -730,26 +733,26 @@ public class DetailActivity extends BaseActivity {
         if (windowsFull == null) {
             windowsFull = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         }
-        if (playerRoot == null)
-            playerRoot = (View) llPlayerFragmentContainer.findViewById(R.id.mVideoView).getParent();
-
-        if (playerParent == null) {
-            playerParent = (ViewGroup) playerRoot.getParent();
-        }
-        if (llLayoutParent == null)
-            llLayoutParent = (ViewGroup) llLayout.getParent();
-
         fullWindows = !fullWindows;
-        // llPlayerFragmentContainer.setLayoutParams(fullWindows ? windowsFull : windowsPreview);
+//        if (playerRoot == null)
+//            playerRoot = (View) llPlayerFragmentContainer.findViewById(R.id.mVideoView).getParent();
+//
+//        if (playerParent == null) {
+//            playerParent = (ViewGroup) playerRoot.getParent();
+//        }
+//        if (llLayoutParent == null)
+//            llLayoutParent = (ViewGroup) llLayout.getParent();
+
+//        if (fullWindows) {
+//            playerParent.removeView(playerRoot);
+//            ((ViewGroup) getWindow().getDecorView()).addView(playerRoot);
+//            llLayoutParent.removeView(llLayout);
+//        } else {
+//            ((ViewGroup) getWindow().getDecorView()).removeView(playerRoot);
+//            playerParent.addView(playerRoot);
+//            llLayoutParent.addView(llLayout);
+//        }
         llPlayerFragmentContainerBlock.setVisibility(fullWindows ? View.GONE : View.VISIBLE);
-        if (fullWindows) {
-            playerParent.removeView(playerRoot);
-            ((ViewGroup) getWindow().getDecorView()).addView(playerRoot);
-            llLayoutParent.removeView(llLayout);
-        } else {
-            ((ViewGroup) getWindow().getDecorView()).removeView(playerRoot);
-            playerParent.addView(playerRoot);
-            llLayoutParent.addView(llLayout);
-        }
+        llPlayerFragmentContainer.setLayoutParams(fullWindows ? windowsFull : windowsPreview);
     }
 }
