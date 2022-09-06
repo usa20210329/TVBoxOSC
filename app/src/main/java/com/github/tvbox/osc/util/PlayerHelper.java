8dec58dd50fc29bcaa2528565a916d711a15458a
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-07 01:03:36 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -141,10 +141,8 @@ public class PlayerHelper {
     public static String getPlayerName(int playType) {
         if (playType == 1) {
             return "IJK播放器";
-        } else if (playType == 2) {           
-            return "Exo播放器";
-        } else if (playType == 6) {
-            return "kodi播放器";              
+        } else if (playType == 2) {
+            return "Exo播放器";           
         } else if (playType == 7) {
             return "当贝播放器";  
         } else if (playType == 8) {
