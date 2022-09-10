7ee4d22dc8ab3438f17531a3be2431d5fb33f56e
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-10 10:42:18 +0800

    Update IjkMediaPlayer.java

--- player/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ player/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -188,12 +188,8 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
                 if (libLoader == null)
                     libLoader = sLocalLibLoader;
                 
-                try {
-                    libLoader.loadLibrary("ijkffmpeg");
-                    libLoader.loadLibrary("ijksdl");
-                } catch (Throwable throwable) {
-
-                }
+                libLoader.loadLibrary("ijkffmpeg");
+                libLoader.loadLibrary("ijksdl");
                 libLoader.loadLibrary("player");
                 mIsLibLoaded = true;
             }
