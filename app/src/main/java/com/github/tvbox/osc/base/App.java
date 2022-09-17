e822f2e17884aaaec61c0723c9c75864747ebd71
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-17 11:20:17 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -51,19 +51,17 @@ public class App extends MultiDexApplication {
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
-       /* putAbsent(HawkConfig.DEBUG_OPEN, false);
-        putAbsent(HawkConfig.API_URL, "asset://cfg.json");
-        putAbsent(HawkConfig.PLAY_TYPE, 1);
-    }*/
-    /**
-     * 如果不存在则设置值.
-     * @param key
-     * @param value
-     */
-    /*private void putAbsent(String key, Object value){
-        if(!Hawk.contains(key)){
-            Hawk.put(key, value);*/
         }
+        //自定义默认配置，硬解，安全dns，缩略图
+        if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
+            Hawk.put(HawkConfig.IJK_CODEC, "硬解码");
+        }
+        if (!Hawk.contains(HawkConfig.DOH_URL)) {
+            Hawk.put(HawkConfig.DOH_URL, 2);
+        }
+        /*if (!Hawk.contains(HawkConfig.SEARCH_VIEW)) {
+            Hawk.put(HawkConfig.SEARCH_VIEW, 2);
+        }*/
     }
 
     public static App getInstance() {
