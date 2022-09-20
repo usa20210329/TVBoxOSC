d808aed9022f7539a5c09d3425149f0e7e4fac12
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-20 17:52:47 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -23,8 +23,10 @@ import com.github.tvbox.osc.subtitle.widget.SimpleSubtitleView;
 import com.github.tvbox.osc.ui.adapter.ParseAdapter;
 import com.github.tvbox.osc.ui.adapter.SelectDialogAdapter;
 import com.github.tvbox.osc.ui.dialog.SelectDialog;
+import com.github.tvbox.osc.util.FastClickCheckUtil;
 import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.PlayerHelper;
+import com.github.tvbox.osc.util.SubtitleHelper;
 import com.orhanobut.hawk.Hawk;
 import com.owen.tvrecyclerview.widget.TvRecyclerView;
 import com.owen.tvrecyclerview.widget.V7LinearLayoutManager;
@@ -181,6 +183,9 @@ public class VodController extends BaseController {
         mVideoSize = findViewById(R.id.tv_videosize);
         mSubtitleView = findViewById(R.id.subtitle_view);
         mZimuBtn = findViewById(R.id.zimu_select);
+        
+        int subtitleTextSize = SubtitleHelper.getTextSize(mActivity);
+        mSubtitleView.setTextSize(subtitleTextSize);        
         
         myHandle=new Handler();
         myRunnable = new Runnable() {
@@ -394,6 +399,7 @@ public class VodController extends BaseController {
             public void onClick(View view) {
                 //myHandle.removeCallbacks(myRunnable);
                 //myHandle.postDelayed(myRunnable, myHandleSeconds);
+                FastClickCheckUtil.check(view);
                 try {
                     String ijk = mPlayerConfig.getString("ijk");
                     List<IJKCode> codecs = ApiConfig.get().getIjkCodes();
@@ -520,6 +526,7 @@ public class VodController extends BaseController {
         mZimuBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                FastClickCheckUtil.check(view);
                 listener.selectSubtitle();
                 hideBottom();
             }
