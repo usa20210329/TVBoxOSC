2f762f5975a1b67a9b60d22667a12eab8e1cc9b0
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-24 00:50:52 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -170,7 +170,7 @@ public class PlayFragment extends BaseLazyFragment {
                 String preProgressKey = progressKey;
                 PlayFragment.this.playNext();
                 if (rmProgress && preProgressKey != null)
-                    CacheManager.delete(MD5.string2MD5(preProgressKey), 0);
+                    CacheManager.delete(MD5.string2MD5(preProgressKey));
             }
 
             @Override
@@ -745,7 +745,7 @@ public class PlayFragment extends BaseLazyFragment {
         String progressKey = mVodInfo.sourceKey + mVodInfo.id + mVodInfo.playFlag + mVodInfo.playIndex;
         //重新播放清除现有进度
         if (reset) {
-            CacheManager.delete(MD5.string2MD5(progressKey), 0);
+            CacheManager.delete(MD5.string2MD5(progressKey));
             CacheManager.delete(MD5.string2MD5(subtitleCacheKey), "");
         }
         if (Thunder.play(vs.url, new Thunder.ThunderCallback() {
