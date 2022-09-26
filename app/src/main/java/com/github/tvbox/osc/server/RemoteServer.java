addfba31aa0a069d150bca0acf46a5d70884a00d
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-26 20:32:14 +0800

    Update RemoteServer.java

--- app/src/main/java/com/github/tvbox/osc/server/RemoteServer.java
+++ app/src/main/java/com/github/tvbox/osc/server/RemoteServer.java
@@ -72,6 +72,16 @@ public class RemoteServer extends NanoHTTPD {
         getRequestList.add(new RawRequestProcess(this.mContext, "/favicon.ico", R.drawable.app_icon, "image/x-icon"));
         getRequestList.add(new RawRequestProcess(this.mContext, "/live.txt", R.raw.live, "text/plain"));
         getRequestList.add(new RawRequestProcess(this.mContext, "/spider.jar", R.raw.spider, "application/java-archive"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/1.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/2.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/3.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/4.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/5.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/6.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/7.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/8.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/9.json", R.raw.spider, "application/json"));
+        getRequestList.add(new RawRequestProcess(this.mContext, "/10.json", R.raw.spider, "application/json"));        
     }
 
     private void addPostRequestProcess() {
