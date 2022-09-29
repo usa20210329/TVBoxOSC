755bb2a262af845b4b03cd69d1b24df51067053c
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-09-29 11:11:38 +0800

    Update OkGoHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -112,6 +112,18 @@ public class OkGoHelper {
         dnsOverHttps = new DnsOverHttps.Builder().client(dohClient).url(dohUrl.isEmpty() ? null : HttpUrl.get(dohUrl)).build();
     }
 
+
+    static OkHttpClient defaultClient = null;
+    static OkHttpClient noRedirectClient = null;
+
+    public static OkHttpClient getDefaultClient() {
+        return defaultClient;
+    }
+
+    public static OkHttpClient getNoRedirectClient() {
+        return noRedirectClient;
+    }
+    
     public static void init() {
         initDnsOverHttps();
         initExoOkHttpClient();
@@ -152,6 +164,12 @@ public class OkGoHelper {
         OkHttpClient okHttpClient = builder.build();
         OkGo.getInstance().setOkHttpClient(okHttpClient);
 
+        defaultClient = okHttpClient;
+
+        builder.followRedirects(false);
+        builder.followSslRedirects(false);
+        noRedirectClient = builder.build();
+        
         initExoOkHttpClient();
         initPicasso(okHttpClient);
     }
