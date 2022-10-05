40aaabbfbe9bfc8a204d7f76c3ca387aa57ac1da
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-10-05 17:04:33 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -1450,14 +1450,15 @@ public class PlayActivity extends BaseActivity {
         public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
             String url = request.getUrl().toString();
             LOG.i("shouldInterceptRequest url:" + url);
-            //css与jpg等无效资源避免请求远程直接返回response
+            //jpg等无效资源避免请求远程直接返回response
             String uselessmMimeType = null;
-            if (url.contains(".css")) {
-                uselessmMimeType = "text/css";
+            if (url.contains(".jpg")) {
             } else if (url.contains(".jpg")) {
                 uselessmMimeType = "image/jpeg";
             } else if (url.contains(".png")) {
                 uselessmMimeType = "image/png";
+            } else if (url.contains(".gif")) {
+                uselessmMimeType = "image/gif";                
             }
             if (uselessmMimeType != null && !uselessmMimeType.isEmpty()) {
                 return new WebResourceResponse(uselessmMimeType, "UTF-8", null);
@@ -1589,14 +1590,14 @@ public class PlayActivity extends BaseActivity {
                 }
                 return null;
             }
-            //css与jpg等无效资源避免请求远程直接返回response
+            //jpg等无效资源避免请求远程直接返回response
             String uselessmMimeType = null;
-            if (url.contains(".css")) {
-                uselessmMimeType = "text/css";
-            } else if (url.contains(".jpg")) {
+            if (url.contains(".jpg")) {
                 uselessmMimeType = "image/jpeg";
             } else if (url.contains(".png")) {
                 uselessmMimeType = "image/png";
+            } else if (url.contains(".gif")) {
+                uselessmMimeType = "image/gif";                
             }
             if (uselessmMimeType != null && !uselessmMimeType.isEmpty()) {
                 return createXWalkWebResourceResponse(uselessmMimeType, "UTF-8", null);
