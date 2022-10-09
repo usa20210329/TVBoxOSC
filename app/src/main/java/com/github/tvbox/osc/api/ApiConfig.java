1ccb92f84e1c8adef905717d2fbd2c0b66573d27
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-10-09 09:44:10 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -19,6 +19,7 @@ import com.github.tvbox.osc.util.DefaultConfig;
 import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.MD5;
 import com.github.tvbox.osc.util.VideoParseRuler;
+import com.github.tvbox.osc.util.LOG;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -31,6 +32,8 @@ import com.orhanobut.hawk.Hawk;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -610,5 +613,92 @@ public class ApiConfig {
             content = content.replace("./", url.substring(0,url.lastIndexOf("/") + 1));
         }
         return content;
+    }
+
+    public void parseLiveTxt(LinkedHashMap<String, LinkedHashMap<String, ArrayList<String>>> linkedHashMap, String str) {
+        ArrayList<String> arrayList;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new StringReader(str));
+            String readLine = bufferedReader.readLine();
+            LinkedHashMap<String, ArrayList<String>> linkedHashMap2 = new LinkedHashMap<>();
+            LinkedHashMap<String, ArrayList<String>> linkedHashMap3 = linkedHashMap2;
+            while (readLine != null) {
+                if (readLine.trim().isEmpty()) {
+                    readLine = bufferedReader.readLine();
+                } else {
+                    String[] split = readLine.split(",");
+                    if (split.length < 2) {
+                        readLine = bufferedReader.readLine();
+                    } else {
+                        if (readLine.contains("#genre#")) {
+                            String trim = split[0].trim();
+                            if (!linkedHashMap.containsKey(trim)) {
+                                linkedHashMap3 = new LinkedHashMap<>();
+                                linkedHashMap.put(trim, linkedHashMap3);
+                            } else {
+                                linkedHashMap3 = linkedHashMap.get(trim);
+                            }
+                        } else {
+                            String trim2 = split[0].trim();
+                            for (String str2 : split[1].trim().split("#")) {
+                                String trim3 = str2.trim();
+                                if (!trim3.isEmpty() && (trim3.startsWith("http") || trim3.startsWith("rtsp") || trim3.startsWith("rtmp"))) {
+                                    if (!linkedHashMap3.containsKey(trim2)) {
+                                        arrayList = new ArrayList<>();
+                                        linkedHashMap3.put(trim2, arrayList);
+                                    } else {
+                                        arrayList = linkedHashMap3.get(trim2);
+                                    }
+                                    if (!arrayList.contains(trim3)) {
+                                        arrayList.add(trim3);
+                                    }
+                                }
+                            }
+                        }
+                        readLine = bufferedReader.readLine();
+                    }
+                }
+            }
+            bufferedReader.close();
+            if (linkedHashMap2.isEmpty()) {
+                return;
+            }
+            linkedHashMap.put("未分组", linkedHashMap2);
+        } catch (Throwable unused) {
+        }
+    }
+
+    public JsonArray jsonifyLiveMap(LinkedHashMap<String, LinkedHashMap<String, ArrayList<String>>> linkedHashMap) {
+        JsonArray jsonarr = new JsonArray();
+        for (String str : linkedHashMap.keySet()) {
+            JsonArray jsonarr2 = new JsonArray();
+            LinkedHashMap<String, ArrayList<String>> linkedHashMap2 = linkedHashMap.get(str);
+            if (!linkedHashMap2.isEmpty()) {
+                for (String str2 : linkedHashMap2.keySet()) {
+                    ArrayList<String> arrayList = linkedHashMap2.get(str2);
+                    if (!arrayList.isEmpty()) {
+                        JsonArray jsonarr3 = new JsonArray();
+                        for (int i = 0; i < arrayList.size(); i++) {
+                            jsonarr3.add(arrayList.get(i));
+                        }
+                        JsonObject jsonobj = new JsonObject();
+                        try {
+                            jsonobj.addProperty("name", str2);
+                            jsonobj.add("urls", jsonarr3);
+                        } catch (Throwable e) {
+                        }
+                        jsonarr2.add(jsonobj);
+                    }
+                }
+                JsonObject jsonobj2 = new JsonObject();
+                try {
+                    jsonobj2.addProperty("group", str);
+                    jsonobj2.add("channels", jsonarr2);
+                } catch (Throwable e) {
+                }
+                jsonarr.add(jsonobj2);
+            }
+        }
+        return jsonarr;
     }    
 }
