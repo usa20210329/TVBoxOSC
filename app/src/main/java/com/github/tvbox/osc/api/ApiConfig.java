8e73391538c110c407bda39417f9e39c0e0e2c42
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-10-12 14:18:54 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -94,25 +94,23 @@ public class ApiConfig {
         try {
             String content = "";
             if (AES.isJson(json)) {
-                return json;          
+                return json;
             } else if (!json.startsWith("2423")) {
                 String[] data = json.split("\\*\\*");
                 content = new String(Base64.decode(data[1], Base64.DEFAULT));
             } else {
                 content = json;
             }
-            if (configKey !=null) {
-                json = AES.ECB(content, configKey); 
-               } 
-            } else { 
-                json = content;
-               } 
             if (content.startsWith("2423")) {
                 String data = content.substring(content.indexOf("2324") + 4, content.length() - 26);
                 content = new String(AES.toBytes(content)).toLowerCase();
                 String key = AES.rightPadding(content.substring(content.indexOf("$#") + 2, content.indexOf("#$")), "0", 16);
                 String iv = AES.rightPadding(content.substring(content.length() - 13), "0", 16);
-                json = AES.CBC(data, key, iv);                  
+                json = AES.CBC(data, key, iv);
+            } else if (configKey !=null) {
+                json = AES.ECB(content, configKey);
+            } else {
+                json = content;
             }
         } catch (Exception e) {
             e.printStackTrace();
