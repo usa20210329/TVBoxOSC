3d68b129ae81f4d810f2611320e50c1a3b5a5714
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-11-06 00:33:41 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -546,7 +546,6 @@ public class PlayActivity extends BaseActivity {
                         }
                         mVideoView.start();
                         mController.resetSpeed();
-                        mController.initLandscapePortraitBtnInfo();
                     }
                 }
             }
