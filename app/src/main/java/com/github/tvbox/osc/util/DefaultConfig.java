07f5a4f13c2bd742d93505aeda7fcc006ce34cd7
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-11-15 01:09:19 +0800

    Update DefaultConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
@@ -49,7 +49,7 @@ public class DefaultConfig {
             }
         }
         if (withMy)
-            data.add(0, new MovieSort.SortData("my0", "主页"));
+            data.add(0, new MovieSort.SortData("my0", "我的"));
         Collections.sort(data);
         return data;
     }
