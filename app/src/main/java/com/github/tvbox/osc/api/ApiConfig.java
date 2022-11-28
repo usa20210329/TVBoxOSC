cbe2fa290c4362891fe9c755f6bba8d17f834758
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-11-28 14:30:54 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -641,14 +641,15 @@ public class ApiConfig {
     public Spider getCSP(SourceBean sourceBean) {
         boolean js = sourceBean.getApi().startsWith("js_") || sourceBean.getApi().endsWith(".js") || sourceBean.getApi().contains(".js?");
         String ext = sourceBean.getExt();
+        if (js) {
+            return JSEngine.getInstance().getSpider(sourceBean);
+        }        
         if (ext.startsWith("asset://")) {
             try {
                 ext = readAssetsText(ext.replace("asset://",""));
             } catch (IOException e) {
                 ext = null;
             }
-        } else if (js) {
-            return JSEngine.getInstance().getSpider(sourceBean);
         } 
         return jarLoader.getSpider(sourceBean.getKey(), sourceBean.getApi(), ext, sourceBean.getJar());
     }
