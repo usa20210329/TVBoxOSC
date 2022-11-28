2014fad3e7a3e3e70182527dcbb54477e2314ff7
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2022-11-28 14:00:11 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -646,10 +646,9 @@ public class ApiConfig {
                 ext = readAssetsText(ext.replace("asset://",""));
             } catch (IOException e) {
                 ext = null;
-            }
-        } else if (js) {
-            return JSEngine.getInstance().getSpider(sourceBean);
-    }else {
+         } else if (js) {
+         return JSEngine.getInstance().getSpider(sourceBean);
+         } else {
         return jarLoader.getSpider(sourceBean.getKey(), sourceBean.getApi(), ext, sourceBean.getJar());
     }
 
