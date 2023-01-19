f50ee5e0ef4ca3b9c8d60472261c1207d38177ff
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-01-19 10:56:27 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -14,6 +14,7 @@ import com.github.tvbox.osc.player.thirdparty.browser;
 import com.github.tvbox.osc.player.thirdparty.MXPlayer;
 import com.github.tvbox.osc.player.thirdparty.ReexPlayer;
 import com.github.tvbox.osc.player.thirdparty.RemoteTVBox;
+import com.github.tvbox.osc.player.thirdparty.VlcPlayer;
 import com.orhanobut.hawk.Hawk;
 
 import org.json.JSONException;
@@ -172,6 +173,7 @@ public class PlayerHelper {
             playersInfo.put(11, "M X播放器");
             playersInfo.put(12, "Reex播放器");
             playersInfo.put(13, "附近TVBox");
+            playersInfo.put(14, "VLC播放器");
             mPlayersInfo = playersInfo;
         }
         return mPlayersInfo;
@@ -191,6 +193,7 @@ public class PlayerHelper {
             playersExist.put(11, MXPlayer.getPackageInfo() != null);
             playersExist.put(12, ReexPlayer.getPackageInfo() != null);  
             playersExist.put(13, RemoteTVBox.getAvalible() != null);
+            playersExist.put(14, VlcPlayer.getPackageInfo() != null);
             mPlayersExistInfo = playersExist;
         }
         return mPlayersExistInfo;
@@ -217,6 +220,10 @@ public class PlayerHelper {
     }
 
     public static Boolean runExternalPlayer(int playerType, Activity activity, String url, String title, String subtitle, HashMap<String, String> headers) {
+        return runExternalPlayer(playerType, activity, url, title, subtitle, headers);
+    }
+
+    public static Boolean runExternalPlayer(int playerType, Activity activity, String url, String title, String subtitle, HashMap<String, String> headers, long progress) {        
         boolean callResult = false;
         switch (playerType) {
             case 7: {
@@ -246,7 +253,11 @@ public class PlayerHelper {
             case 13: {
                 callResult = RemoteTVBox.run(activity, url, title, subtitle, headers);
                 break;
-            }                
+            }
+            case 14: {
+                callResult = VlcPlayer.run(activity, url, title, subtitle, progress);
+                break;
+            }               
         }
         return callResult;
     }
