bbdda624a8cc0f9db56baac6996534a0ac2bae2d
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-06-17 19:07:04 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -691,9 +691,9 @@ public class PlayFragment extends BaseLazyFragment {
     }
     void startPlayUrl(String url, HashMap<String, String> headers) {      
         LOG.i("playUrl:" + url);
-        if(autoRetryCount>0 && url.contains(".m3u8")){
-            url="http://home.jundie.top:666/unBom.php?m3u8="+url;
-        }
+        //if(autoRetryCount>0 && url.contains(".m3u8")){
+        //    url="http://home.jundie.top:666/unBom.php?m3u8="+url;
+        //}
         String finalUrl = url;    
         if (mActivity == null) return;
         requireActivity().runOnUiThread(new Runnable() {
