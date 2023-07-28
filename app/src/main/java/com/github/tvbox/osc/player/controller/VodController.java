0a02ccdb0a27438a806a76974fff4122006a86a6
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-07-29 01:37:37 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -955,4 +955,10 @@ public class VodController extends BaseController {
         }
         return false;
     }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mHandler.removeCallbacks(myRunnable2);
+    }    
 }
