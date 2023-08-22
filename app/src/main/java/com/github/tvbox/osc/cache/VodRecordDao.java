ce2b9d95adc4dc1f2511c60cf3a127eb15a0812e
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-08-22 20:19:05 +0800

    Update VodRecordDao.java

--- app/src/main/java/com/github/tvbox/osc/cache/VodRecordDao.java
+++ app/src/main/java/com/github/tvbox/osc/cache/VodRecordDao.java
@@ -29,7 +29,10 @@ public interface VodRecordDao {
     
     @Query("select count(*) from vodRecord")
     int getCount();
-
+    
+    @Query("DELETE FROM vodRecord")
+    void deleteAll();
+    
     /**
      * 保留最新指定条数, 其他删除.
      * @param size 保留条数
