f2e6f61e2bc0f41d6bad39414ba57f42dde0c659
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-08-23 19:24:45 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -691,16 +691,6 @@ public class PlayActivity extends BaseActivity {
         if(autoRetryCount>1 && url.contains(".m3u8")){
            url="http://home.jundie.top:666/unBom.php?m3u8="+url;//尝试去bom头再次播放
         }
-        if (url.startsWith("data:application/dash+xml;base64,")) {
-            PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg, 2);
-            App.getInstance().setDashData(url.split("base64,")[1]);
-            url = ControlManager.get().getAddress(true) + "dash/proxy.mpd";
-        } else if (url.contains(".mpd") || url.contains("type=mpd")) {
-            PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg, 2);
-        } else {
-            PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg);            
-        }        
-        String finalUrl = url; 
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -708,7 +698,7 @@ public class PlayActivity extends BaseActivity {
                 if (mVideoView != null) {
                     mVideoView.release();
 
-                    if (finalUrl != null) {
+                    if (url != null) {
                         try {
                             int playerType = mVodPlayerCfg.getInt("pl");
                             if (playerType >= 7) {
@@ -725,6 +715,16 @@ public class PlayActivity extends BaseActivity {
                             e.printStackTrace();
                         }
                         hideTip();
+                        String finalUrl = url;
+                        if (finalUrl.startsWith("data:application/dash+xml;base64,")) {
+                            PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg, 2);
+                            App.getInstance().setDashData(finalUrl.split("base64,")[1]);
+                            finalUrl = ControlManager.get().getAddress(true) + "dash/proxy.mpd";
+                        } else if (finalUrl.contains(".mpd") || finalUrl.contains("type=mpd")) {
+                            PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg, 2);
+                        } else {
+                            PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg);
+                        }                        
                         //PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg);
                         mVideoView.setProgressKey(progressKey);
                         if (headers != null) {
