e9549fc15d5243614913975f4f77ee4ad11d9e79
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-09-07 01:12:06 +0800

    Update HomeHotVodAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -60,11 +60,12 @@ public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
         if (!TextUtils.isEmpty(item.pic)) {
             Picasso.get()
                     .load(DefaultConfig.checkReplaceProxy(item.pic))
-                    .transform(new RoundTransformation(MD5.string2MD5(item.pic + "position=" + helper.getLayoutPosition()))
+                    .transform(new RoundTransformation(MD5.string2MD5(item.pic))
                             .centerCorp(true)
                             .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
                             .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                     .placeholder(R.drawable.img_loading_placeholder)
+                    .noFade()
                     .error(R.drawable.img_loading_placeholder)
                     .into(ivThumb);
         } else {
