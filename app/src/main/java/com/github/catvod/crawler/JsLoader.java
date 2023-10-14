3c1daaf0a1ac3c96813f3ab6954ab31f5d3fed9c
usa20210329 <108717020+usa20210329@users.noreply.github.com>
2023-10-14 10:55:22 +0800

    Update JsLoader.java

--- app/src/main/java/com/github/catvod/crawler/JsLoader.java
+++ app/src/main/java/com/github/catvod/crawler/JsLoader.java
@@ -1,49 +1,49 @@
 package com.github.catvod.crawler;
 
-import android.content.Context;
 
 import com.github.tvbox.osc.base.App;
-import com.github.tvbox.osc.util.MD5;
-import com.lzy.okgo.OkGo;
 
-import org.json.JSONObject;
+import com.github.tvbox.osc.util.LOG;
+import com.github.tvbox.osc.util.MD5;
+
+import com.github.tvbox.osc.util.js.JsSpider;
+import com.lzy.okgo.OkGo;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import dalvik.system.DexClassLoader;
 import okhttp3.Response;
 
-public class JarLoader {
-    private ConcurrentHashMap<String, DexClassLoader> classLoaders = new ConcurrentHashMap<>();
-    private ConcurrentHashMap<String, Method> proxyMethods = new ConcurrentHashMap<>();
-    private ConcurrentHashMap<String, Spider> spiders = new ConcurrentHashMap<>();
-    private volatile String recentJarKey = "";
+public class JsLoader {
+    private static ConcurrentHashMap<String, Spider> spiders = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, Class<?>> classs = new ConcurrentHashMap<>();
 
-    /**
-     * 不要在主线程调用我
-     *
-     * @param cache
-     */
-    public boolean load(String cache) {
+    public static void load() {
+        for (Spider spider : spiders.values()){
+            spider.cancelByTag();
+            spider.destroy();
+        }
         spiders.clear();
-        recentJarKey = "main";
-        proxyMethods.clear();
-        classLoaders.clear();
-        return loadClassLoader(cache, "main");
+        classs.clear();
+    }
+
+    public static void stopAll() {
+        for (Spider spider : spiders.values()){
+            spider.cancelByTag();
+        }
     }
 
     private boolean loadClassLoader(String jar, String key) {
         boolean success = false;
+        Class<?> classInit = null;
         try {
-            File cacheDir = new File(App.getInstance().getCacheDir().getAbsolutePath() + "/catvod_csp");
+            File cacheDir = new File(App.getInstance().getCacheDir().getAbsolutePath() + "/catvod_jsapi");
             if (!cacheDir.exists())
                 cacheDir.mkdirs();
             DexClassLoader classLoader = new DexClassLoader(jar, cacheDir.getAbsolutePath(), null, App.getInstance().getClassLoader());
@@ -51,19 +51,10 @@ public class JarLoader {
             int count = 0;
             do {
                 try {
-                    Class classInit = classLoader.loadClass("com.github.catvod.spider.Init");
+                    classInit = classLoader.loadClass("com.github.catvod.js.Method");
                     if (classInit != null) {
-                        Method method = classInit.getMethod("init", Context.class);
-                        method.invoke(null, App.getInstance());
-                        System.out.println("自定义爬虫代码加载成功!");
+                        System.out.println("自定义jsapi加载成功!");
                         success = true;
-                        try {
-                            Class proxy = classLoader.loadClass("com.github.catvod.spider.Proxy");
-                            Method mth = proxy.getMethod("proxy", Map.class);
-                            proxyMethods.put(key, mth);
-                        } catch (Throwable th) {
-
-                        }
                         break;
                     }
                     Thread.sleep(200);
@@ -74,7 +65,7 @@ public class JarLoader {
             } while (count < 5);
 
             if (success) {
-                classLoaders.put(key, classLoader);
+                classs.put(key, classInit);
             }
         } catch (Throwable th) {
             th.printStackTrace();
@@ -82,14 +73,14 @@ public class JarLoader {
         return success;
     }
 
-    private DexClassLoader loadJarInternal(String jar, String md5, String key) {
-        if (classLoaders.contains(key))
-            return classLoaders.get(key);
+    private Class<?> loadJarInternal(String jar, String md5, String key) {
+        if (classs.contains(key))
+            return classs.get(key);
         File cache = new File(App.getInstance().getFilesDir().getAbsolutePath() + "/" + key + ".jar");
         if (!md5.isEmpty()) {
             if (cache.exists() && MD5.getFileMd5(cache).equalsIgnoreCase(md5)) {
                 loadClassLoader(cache.getAbsolutePath(), key);
-                return classLoaders.get(key);
+                return classs.get(key);
             }
         }
         try {
@@ -111,87 +102,47 @@ public class JarLoader {
                 }
             }
             loadClassLoader(cache.getAbsolutePath(), key);
-            return classLoaders.get(key);
+            return classs.get(key);
         } catch (Throwable e) {
             e.printStackTrace();
         }
         return null;
     }
+    private volatile String recentJarKey = "";
 
-    public Spider getSpider(String key, String cls, String ext, String jar) {
-        String clsKey = cls.replace("csp_", "");
-        String jarUrl = "";
-        String jarMd5 = "";
-        String jarKey = "";
-        if (jar.isEmpty()) {
-            jarKey = "main";
-        } else {
+
+    public Spider getSpider(String key, String api, String ext, String jar) {
+        Class<?> classLoader = null;
+        if (!jar.isEmpty()) {
             String[] urls = jar.split(";md5;");
-            jarUrl = urls[0];
-            jarKey = MD5.string2MD5(jarUrl);
-            jarMd5 = urls.length > 1 ? urls[1].trim() : "";
-        }
-        recentJarKey = jarKey;
-        if (spiders.containsKey(key))
-            return spiders.get(key);
-        DexClassLoader classLoader = null;
-        if (jarKey.equals("main"))
-            classLoader = classLoaders.get("main");
-        else {
+            String jarUrl = urls[0];
+            String jarKey = MD5.string2MD5(jarUrl);
+            String jarMd5 = urls.length > 1 ? urls[1].trim() : "";
             classLoader = loadJarInternal(jarUrl, jarMd5, jarKey);
         }
-        if (classLoader == null)
-            return new SpiderNull();
+        recentJarKey = key;
+        if (spiders.containsKey(key))
+            return spiders.get(key);
         try {
-            Spider sp = (Spider) classLoader.loadClass("com.github.catvod.spider." + clsKey).newInstance();
+            Spider sp = new JsSpider(key, api, classLoader);
             sp.init(App.getInstance(), ext);
-//            if (!jar.isEmpty()) {
-//                sp.homeContent(false); // 增加此行 应该可以解决部分写的有问题源的历史记录问题 但会增加这个源的首次加载时间 不需要可以已删掉
-//            }
             spiders.put(key, sp);
             return sp;
         } catch (Throwable th) {
             th.printStackTrace();
+            LOG.e("QuJS", th);
         }
         return new SpiderNull();
     }
 
-    public JSONObject jsonExt(String key, LinkedHashMap<String, String> jxs, String url) {
+    public Object[] proxyInvoke(Map<String, String> params) {
         try {
-            DexClassLoader classLoader = classLoaders.get("main");
-            String clsKey = "Json" + key;
-            String hotClass = "com.github.catvod.parser." + clsKey;
-            Class jsonParserCls = classLoader.loadClass(hotClass);
-            Method mth = jsonParserCls.getMethod("parse", LinkedHashMap.class, String.class);
-            return (JSONObject) mth.invoke(null, jxs, url);
-        } catch (Throwable th) {
-            th.printStackTrace();
-        }
-        return null;
-    }
-
-    public JSONObject jsonExtMix(String flag, String key, String name, LinkedHashMap<String, HashMap<String, String>> jxs, String url) {
-        try {
-            DexClassLoader classLoader = classLoaders.get("main");
-            String clsKey = "Mix" + key;
-            String hotClass = "com.github.catvod.parser." + clsKey;
-            Class jsonParserCls = classLoader.loadClass(hotClass);
-            Method mth = jsonParserCls.getMethod("parse", LinkedHashMap.class, String.class, String.class, String.class);
-            return (JSONObject) mth.invoke(null, jxs, name, flag, url);
-        } catch (Throwable th) {
-            th.printStackTrace();
-        }
-        return null;
-    }
-
-    public Object[] proxyInvoke(Map params) {
-        try {
-            Method proxyFun = proxyMethods.get(recentJarKey);
+            Spider proxyFun = spiders.get(recentJarKey);
             if (proxyFun != null) {
-                return (Object[]) proxyFun.invoke(null, params);
+                return proxyFun.proxyLocal(params);
             }
         } catch (Throwable th) {
-
+            LOG.e("proxyInvoke", th);
         }
         return null;
     }
